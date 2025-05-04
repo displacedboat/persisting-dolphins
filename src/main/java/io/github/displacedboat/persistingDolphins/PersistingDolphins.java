8db22d0578c350b4f7ac644d662854172e05f65a
tyler <thoff17@hotmail.com>
2025-05-03 22:58:56 -0400

    prevent name-tagged dolphins from dying from drowning, suffocation, or drying out

--- src/main/java/io/github/displacedboat/persistingDolphins/PersistingDolphins.java
+++ src/main/java/io/github/displacedboat/persistingDolphins/PersistingDolphins.java
@@ -1,17 +1,33 @@
 package io.github.displacedboat.persistingDolphins;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PersistingDolphins extends JavaPlugin {
+public final class PersistingDolphins extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageEvent e) {
+        // Prevent drowning, dryout, or suffocation damage to a named dolphin
+        Entity entity = e.getEntity();
+        EntityDamageEvent.DamageCause cause = e.getCause();
+
+        // There may be a more performant order to do this checks in
+        if ((entity.getType() == EntityType.DOLPHIN)
+                && (entity.getCustomName() != null)
+                && ((cause == EntityDamageEvent.DamageCause.SUFFOCATION)
+                    || (cause == EntityDamageEvent.DamageCause.DROWNING)
+                    || (cause == EntityDamageEvent.DamageCause.DRYOUT))) {
+            e.setCancelled(true);
+        }
     }
 }
